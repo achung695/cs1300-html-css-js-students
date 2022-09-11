aa7bc9e0e8bec1223932b6d1f4c3e998ae05b522
Ashley Chung <ashleychung@ashleys-mbp-6.devices.brown.edu>
2022-09-11 17:57:27 -0400

    finished lab

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const apiRequest = async () => {
     }
   });
 
-  // console.log(response);
+  console.log(response);
 
   // Return the response in JSON format
   return response.json();
@@ -26,14 +26,29 @@ const updatePage = async () => {
 
   // Make API request and get an array of fruit objects
   const fruitsArray = await apiRequest();
-  // console.log(fruitsArray);
+  
 
   // TODO: Use either `map` and/or `filter` to extract some data from the array of fruit objects
   // For example, find "name of all fruits whose sugar > 15",
-
+  const filteredFruitsArray = fruitsArray.filter((fruit, i) => {
+    return fruit.nutritions.sugar < 15;
+  })
+  console.log(filteredFruitsArray);
   // TODO: Create a new HTML element to display your data
 
+  const mappedFruitsArray = filteredFruitsArray.map(element => {
+    return element.name
+  })
+
+  console.log(mappedFruitsArray)
+
+ 
+  const newElement = document.createElement('div');
+  newElement.innerHTML = mappedFruitsArray;
   // TODO: Append your new element to the page
+  const existingElement = document.getElementById('cs1300-gallery');
+  existingElement.append(newElement);
+  
 
 }
 
